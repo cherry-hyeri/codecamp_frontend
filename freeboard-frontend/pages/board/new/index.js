92f124d438e9d86abbcb07631fda7cb1d5ac831e
he_hea <rnd903@gmail.com>
2023-11-03 21:10:03 +0900

    포트폴리오 02

--- freeboard-frontend/pages/board/new/index.js
+++ freeboard-frontend/pages/board/new/index.js
@@ -1,134 +1,134 @@
-// import { useState } from "react";
-// import * as S from "../../../styles/boardsNew";
+import { useState } from "react";
+import * as S from "../../../styles/boardsNew";
 
-// export default function BoardNewPage() {
-//   const [writer, setWriter] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [title, setTitle] = useState("");
-//   const [contents, setContents] = useState("");
+export default function BoardNewPage() {
+  const [writer, setWriter] = useState("");
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
 
-//   const [writerError, setWriterError] = useState("");
-//   const [passwordError, setPasswordError] = useState("");
-//   const [titleError, setTitleError] = useState("");
-//   const [contentsError, setContentsError] = useState("");
+  const [writerError, setWriterError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [contentsError, setContentsError] = useState("");
 
-//   const onChangeWriter = (event) => {
-//     setWriter(event.target.value);
-//     if (event.target.value !== "") {
-//       setWriterError("");
-//     }
-//   };
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+    if (event.target.value !== "") {
+      setWriterError("");
+    }
+  };
 
-//   const onChangePassword = (event) => {
-//     setPassword(event.target.value);
-//     if (event.target.value !== "") {
-//       setPasswordError("");
-//     }
-//   };
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value !== "") {
+      setPasswordError("");
+    }
+  };
 
-//   const onChangeTitle = (event) => {
-//     setTitle(event.target.value);
-//     if (event.target.value !== "") {
-//       setTitleError("");
-//     }
-//   };
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+    if (event.target.value !== "") {
+      setTitleError("");
+    }
+  };
 
-//   const onChangeContents = (event) => {
-//     setContents(event.target.value);
-//     if (event.target.value !== "") {
-//       setContentsError("");
-//     }
-//   };
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if (event.target.value !== "") {
+      setContentsError("");
+    }
+  };
 
-//   function onClickSubmit() {
-//     if (!writer) {
-//       setWriterError("작성자를 입력해 주세요.");
-//     }
-//     if (!password) {
-//       setPasswordError("비밀번호를 입력해 주세요.");
-//     }
-//     if (!title) {
-//       setTitleError("제목을 입력해 주세요.");
-//     }
-//     if (!contents) {
-//       setContentsError("내용을 입력해 주세요.");
-//     }
-//     if (writer && password && title && contents) {
-//       alert("게시글이 등록되었습니다.");
-//     }
-//   }
+  function onClickSubmit() {
+    if (!writer) {
+      setWriterError("작성자를 입력해 주세요.");
+    }
+    if (!password) {
+      setPasswordError("비밀번호를 입력해 주세요.");
+    }
+    if (!title) {
+      setTitleError("제목을 입력해 주세요.");
+    }
+    if (!contents) {
+      setContentsError("내용을 입력해 주세요.");
+    }
+    if (writer && password && title && contents) {
+      alert("게시글이 등록되었습니다.");
+    }
+  }
 
-//   return (
-//     <>
-//       <S.Wrapper>
-//         <S.Title>게시글 등록</S.Title>
-//         <S.WriterWrapper>
-//           <S.InputWrapper>
-//             <S.Label>작성자</S.Label>
-//             <S.Writer
-//               type="text"
-//               placeholder="이름을 적어주세요."
-//               onChange={onChangeWriter}
-//             />
-//             <S.Error>{writerError}</S.Error>
-//           </S.InputWrapper>
-//           <S.InputWrapper>
-//             <S.Label>비밀번호</S.Label>
-//             <S.Password
-//               type="password"
-//               placeholder="비밀번호를 작성해주세요."
-//               onChange={onChangePassword}
-//             />
-//             <S.Error>{passwordError}</S.Error>
-//           </S.InputWrapper>
-//         </S.WriterWrapper>
-//         <S.InputWrapper>
-//           <S.Label>제목</S.Label>
-//           <S.Subject
-//             type="text"
-//             placeholder="제목을 작성해주세요."
-//             onChange={onChangeTitle}
-//           />
-//           <S.Error>{titleError}</S.Error>
-//         </S.InputWrapper>
-//         <S.InputWrapper>
-//           <S.Label>내용</S.Label>
-//           <S.Contents
-//             placeholder="내용을 작성해주세요."
-//             onChange={onChangeContents}
-//           />
-//           <S.Error>{contentsError}</S.Error>
-//         </S.InputWrapper>
-//         <S.InputWrapper>
-//           <S.Label>주소</S.Label>
-//           <S.ZipcodeWrapper>
-//             <S.Zipcode placeholder="07250" />
-//             <S.SearchButton>우편번호 검색</S.SearchButton>
-//           </S.ZipcodeWrapper>
-//           <S.Address />
-//           <S.Address />
-//         </S.InputWrapper>
-//         <S.InputWrapper>
-//           <S.Label>유튜브</S.Label>
-//           <S.Youtube placeholder="링크를 복사해주세요." />
-//         </S.InputWrapper>
-//         <S.ImageWrapper>
-//           <S.Label>사진첨부</S.Label>
-//           <S.UploadButton>+</S.UploadButton>
-//           <S.UploadButton>+</S.UploadButton>
-//           <S.UploadButton>+</S.UploadButton>
-//         </S.ImageWrapper>
-//         <S.OptionWrapper>
-//           <S.Label>메인설정</S.Label>
-//           <S.RadioButton type="radio" id="youtube" name="radio-button" />
-//           <S.RadioLabel htmlFor="youtube">유튜브</S.RadioLabel>
-//           <S.RadioButton type="radio" id="image" name="radio-button" />
-//           <S.RadioLabel htmlFor="image">사진</S.RadioLabel>
-//         </S.OptionWrapper>
-//         <S.ButtonWrapper>
-//           <S.SubmitButton onClick={onClickSubmit}>등록하기</S.SubmitButton>
-//         </S.ButtonWrapper>
-//       </S.Wrapper>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <S.Wrapper>
+        <S.Title>게시글 등록</S.Title>
+        <S.WriterWrapper>
+          <S.InputWrapper>
+            <S.Label>작성자</S.Label>
+            <S.Writer
+              type="text"
+              placeholder="이름을 적어주세요."
+              onChange={onChangeWriter}
+            />
+            <S.Error>{writerError}</S.Error>
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.Label>비밀번호</S.Label>
+            <S.Password
+              type="password"
+              placeholder="비밀번호를 작성해주세요."
+              onChange={onChangePassword}
+            />
+            <S.Error>{passwordError}</S.Error>
+          </S.InputWrapper>
+        </S.WriterWrapper>
+        <S.InputWrapper>
+          <S.Label>제목</S.Label>
+          <S.Subject
+            type="text"
+            placeholder="제목을 작성해주세요."
+            onChange={onChangeTitle}
+          />
+          <S.Error>{titleError}</S.Error>
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <S.Label>내용</S.Label>
+          <S.Contents
+            placeholder="내용을 작성해주세요."
+            onChange={onChangeContents}
+          />
+          <S.Error>{contentsError}</S.Error>
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <S.Label>주소</S.Label>
+          <S.ZipcodeWrapper>
+            <S.Zipcode placeholder="07250" />
+            <S.SearchButton>우편번호 검색</S.SearchButton>
+          </S.ZipcodeWrapper>
+          <S.Address />
+          <S.Address />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <S.Label>유튜브</S.Label>
+          <S.Youtube placeholder="링크를 복사해주세요." />
+        </S.InputWrapper>
+        <S.ImageWrapper>
+          <S.Label>사진첨부</S.Label>
+          <S.UploadButton>+</S.UploadButton>
+          <S.UploadButton>+</S.UploadButton>
+          <S.UploadButton>+</S.UploadButton>
+        </S.ImageWrapper>
+        <S.OptionWrapper>
+          <S.Label>메인설정</S.Label>
+          <S.RadioButton type="radio" id="youtube" name="radio-button" />
+          <S.RadioLabel htmlFor="youtube">유튜브</S.RadioLabel>
+          <S.RadioButton type="radio" id="image" name="radio-button" />
+          <S.RadioLabel htmlFor="image">사진</S.RadioLabel>
+        </S.OptionWrapper>
+        <S.ButtonWrapper>
+          <S.SubmitButton onClick={onClickSubmit}>등록하기</S.SubmitButton>
+        </S.ButtonWrapper>
+      </S.Wrapper>
+    </>
+  );
+}
