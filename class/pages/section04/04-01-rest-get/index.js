d7f615dcb4df6c45f1e535db15bcbbbd4a5669af
he_hea <rnd903@gmail.com>
2023-11-06 13:38:43 +0900

    04-01-graphQL 셋팅

--- class/pages/section04/04-01-rest-get/index.js
+++ class/pages/section04/04-01-rest-get/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import ExamplePage from "../../section01/01-01-example";
 
 export default function RestGetPage() {
   function onClickAsync() {
@@ -6,15 +7,22 @@ export default function RestGetPage() {
     console.log(result); //결과 : Promise
   }
 
-  async function onClickSync() {
+  //   async function onClickSync() {
+  //     const result = await axios.get("https://koreanjson.com/posts/1"); => 함수 중복 선언 문제
+  //     console.log(result); // 제대로 된 결과 { title: "....", }
+  //   }
+
+  const onClickSync = async () => {
     const result = await axios.get("https://koreanjson.com/posts/1");
     console.log(result); // 제대로 된 결과 { title: "....", }
-  }
+    console.log(result.data.title); //정당의~~
+  };
 
   return (
     <>
       <button onClick={onClickAsync}>REST-API(비동기) 요청하기</button>
       <button onClick={onClickSync}>REST-API(동기) 요청하기</button>
+      <ExamplePage />
     </>
   );
 }
