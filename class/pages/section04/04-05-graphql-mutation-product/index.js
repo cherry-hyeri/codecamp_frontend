29b3871f5e9bc1ecdbed2d5a624f152f935ca943
he_hea <rnd903@gmail.com>
2023-11-06 22:05:21 +0900

    04-05-graphql-mutation-product

---
+++ class/pages/section04/04-05-graphql-mutation-product/index.js
@@ -0,0 +1,35 @@
+import { useMutation, gql } from "@apollo/client";
+
+const CREATEPRODUCT = gql`
+  mutation createProduct( # 변수의 타입 적는 곳
+    $seller: String
+    $createProductInput: CreateProductInput!
+  ) {
+    # 실제 우리가 전달할 변수 적는 곳 V
+    createProduct(seller: $seller, createProductInput: $createProductInput) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  const [createProduct] = useMutation(CREATEPRODUCT);
+
+  const onClickSubmit = async () => {
+    const result = await createProduct({
+      variables: {
+        seller: "micky",
+        createProductInput: {
+          name: "mouse",
+          detail: "cute",
+          price: 1000,
+        },
+      },
+    });
+    console.log(result);
+  };
+
+  return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>;
+}
