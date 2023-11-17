121c81aad0649b7cc8710b42fb07ef4654fd9c8a
he_hea <rnd903@gmail.com>
2023-11-17 18:24:38 +0900

    09-04-boards 수정 02

---
+++ class/src/components/units/board/09-write2/BoardWrite.container.js
@@ -0,0 +1,73 @@
+import { useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { 나의그래프큐엘셋팅, UPDATE_BOARD } from "./BoardWrite.queries";
+
+export default function BoardWrite(props) {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(UPDATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        // variables = $ 역할을 함
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(`/section09/09-04-boards/${result.data.createBoard.number}`);
+  };
+
+  const onChangeUpdate = async () => {
+    const myvariables = {
+      number: Number(router.query.number),
+    };
+    if (writer) myvariables.writer = writer;
+    if (title) myvariables.title = title;
+    if (contents) myvariables.contents = contents;
+
+    //여기서 수정하기
+    const result = await updateBoard({
+      variables: myvariables,
+    });
+    console.log("update :", result);
+    router.push(`/section09/09-04-boards/${result.data.updateBoard.number}`);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      <div>0000여기는 컴포넌트00000</div>
+      <BoardWriteUI
+        onClickSubmit={onClickSubmit}
+        onChangeUpdate={onChangeUpdate}
+        onChangeWriter={onChangeWriter}
+        onChangeTitle={onChangeTitle}
+        onChangeContents={onChangeContents}
+        isEdit={props.isEdit}
+        data={props.data} //undefined 이거나, data 이거나 둘 중 하나
+      />
+      <div>0000여기는 컴포넌트00000</div>
+    </>
+  );
+}
---
+++ class/src/components/units/board/09-write2/BoardWrite.presenter.js
@@ -0,0 +1,35 @@
+import { RedInput, BlueButton } from "./BoardWrite.style";
+
+export default function BoardWriteUI(props) {
+  return (
+    <>
+      <div>aaaa여기는 프리젠터aaaa</div>
+      <div>
+        작성자 :{" "}
+        <RedInput
+          type="text"
+          onChange={props.onChangeWriter}
+          defaultValue={props.data?.fetchBoard.writer}
+        />
+        제목 :{" "}
+        <input
+          type="text"
+          onChange={props.onChangeTitle}
+          defaultValue={props.data?.fetchBoard.title}
+        />
+        내용 :{" "}
+        <input
+          type="text"
+          onChange={props.onChangeContents}
+          defaultValue={props.data?.fetchBoard.contents}
+        />
+        <BlueButton
+          onClick={props.isEdit ? props.onChangeUpdate : props.onClickSubmit}
+        >
+          {props.isEdit ? "수정" : "등록"}하기
+        </BlueButton>
+      </div>
+      <div>aaaa여기는 프리젠터aaaa</div>
+    </>
+  );
+}
