d8da15bd3ef84beaa3557b58d1c66292ecb1a966
he_hea <rnd903@gmail.com>
2023-11-26 16:24:43 +0900

    eslint && prettier

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const client = new ApolloClient({
     uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), //컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
+    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기
   });
 
   return (
