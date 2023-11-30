f4ab6cb8d32cb386fa534d4693e847772a32081e
he_hea <rnd903@gmail.com>
2023-11-30 17:11:15 +0900

    13-04-prev-state

---
+++ class/pages/section13/13-04-prev-state-modal-custom-refactoring/index.tsx
@@ -0,0 +1,34 @@
+import { Modal } from "antd";
+import { useState } from "react";
+import DaumPostcodeEmbed from "react-daum-postcode";
+import type { Address } from "react-daum-postcode";
+
+export default function ModalAlertPage(): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggleModal = (): void => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleComplete = (data: Address): void => {
+    console.log(data);
+    onToggleModal();
+  };
+
+  return (
+    <>
+      <button onClick={onToggleModal}>open modal</button>
+      {/* 모달 종료 방식 - 1. 모달 숨기는 방법(내용이 남음) */}
+      {/* <Modal open={isOpen} onOk={handleOk} onCancel={handleCancel}>
+        <DaumPostcodeEmbed onComplete={handleComplete} />
+      </Modal> */}
+
+      {/* 모달 종료 방식 - 2. 모달 삭제하는 방법(내용이 리셋됨) */}
+      {isOpen && (
+        <Modal open={true} onOk={onToggleModal} onCancel={onToggleModal}>
+          <DaumPostcodeEmbed onComplete={handleComplete} />
+        </Modal>
+      )}
+    </>
+  );
+}
